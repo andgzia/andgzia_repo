35022c811b5c78754fd45bfece6067d66084fd36
andgzia <zhiabuss.ceo@gmail.com>
2016-03-12 17:22:52 +0700

    add jquerypp.custom.js

---
+++ js/jquerypp.custom.js
@@ -0,0 +1,17 @@
+(function()) {
+ 
+    var event = JQuery.event,
+ 
+        //helper that finds handlers by type and calls back a function, this is basically handle
+		// events - the events object
+		// types - an array of event types to look for
+		// callback(type, handlerFunc, selector) - a callback
+		// selector - an optional selector to filter with, if there, matches by selector
+		//     if null, matches anything, otherwise, matches with no selector
+ 
+    findHelper = function(events, types, callback, selector) {
+    
+    
+}
+ 
+ }
